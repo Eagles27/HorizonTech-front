8a080b1038769481bb8fce4e458c86303a61e126
clemencebellet <78761529+clemencebellet@users.noreply.github.com>
2024-01-21 18:07:21 +0100

    feat: page 404 (#43)
    * feat(page404): creation page 404
    
    * fix(page404): modification configuration de la route
    
    * fix(page404): color text

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import HomeModel from "./lib/pages/home/home.model";
 import MetierPediaModel from "./lib/pages/metier-pedia/metierpedia.model";
+import Error404Model from "./lib/pages/error-page/error404.model";
 import SignInModel from "./lib/pages/signin/signIn.model";
 import ProtectedRoute from "./utils/functions/protectedRoute";
 import { TRootState } from "./store/store";
@@ -24,6 +25,7 @@ const App: React.FC = () => {
         }
       />
       <Route path="/metier-pedia" element={<MetierPediaModel />} />
+      <Route path="/*" element={<Error404Model />} />
       <Route path="/signup-signin" element={<SignInModel />} />
       <Route
         path="/profile"
