675c263679aa84d6b661c6275ed55a9d2b7da986
Maxime <maxime.gillen@outlook.fr>
2024-01-22 15:52:12 +0100

    fix(signin): redirect already logged-in users from login to profile page

--- src/lib/pages/signin/signIn.model.tsx
+++ src/lib/pages/signin/signIn.model.tsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SignInView from "./signIn.view";
+import { useSelector } from "react-redux";
+import { TRootState } from "../../../store/store";
+import { useNavigate } from "react-router-dom";
 
 const SignInModel: React.FC = () => {
   const [signUpView, setSignUpView] = useState(true);
+  const isAuth = useSelector((state: TRootState) => state.userSlice.isAuth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuth) {
+      navigate("/profile");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuth]);
 
   return <SignInView signUpView={signUpView} setSignUpView={setSignUpView} />;
 };
